139ce278a603bc80f801d9a34b30abcbe2e68724
smithdk <smithdk.private@gmail.com>
2021-01-18 21:46:41 +0500

    Добавил в скетч обработку замыкания по прерываниям

--- ESP8266/water_meter/water_meter.ino
+++ ESP8266/water_meter/water_meter.ino
@@ -1,37 +1,55 @@
 /*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
 */
+#include "Bounce2.h";
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#define COOL_WATER_PIN 12
+#define HOT_WATER_PIN 13
+int cool_water_counter = 0;
+int hot_water_counter = 0;
+long lastDebounce_cool = 0;
+long lastDebounce_hot = 0;
+long debounceDelay = 500; 
+
+void ICACHE_RAM_ATTR cool_trigger() {
+ if ((millis() - lastDebounce_cool) > debounceDelay) 
+  { 
+    cool_water_counter ++;
+    Serial.println("cool_water_counter: " + (String)(cool_water_counter));
+    lastDebounce_cool = millis();
+  }
 }
 
-// the loop function runs over and over again forever
+void ICACHE_RAM_ATTR hot_trigger() {  
+ if ((millis() - lastDebounce_hot) > debounceDelay) 
+  { 
+    hot_water_counter ++;
+    Serial.println("hot_water_counter: " + (String)(hot_water_counter));
+    lastDebounce_hot = millis();
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Water meter satrdted v 1.0.0");
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(COOL_WATER_PIN, INPUT_PULLUP);
+  pinMode(HOT_WATER_PIN, INPUT_PULLUP);
+  digitalWrite(COOL_WATER_PIN, HIGH);
+  digitalWrite(HOT_WATER_PIN, HIGH);
+  attachInterrupt(digitalPinToInterrupt(COOL_WATER_PIN), cool_trigger, FALLING);
+  attachInterrupt(digitalPinToInterrupt(HOT_WATER_PIN),  hot_trigger,  FALLING);
+}
+
+
+
+
+
+
+
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  delay(500);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
